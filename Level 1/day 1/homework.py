0f246617a800318237863f458b0ae367474d17ce
dachi14purcx <dachipurtskhvanidze2@gmail.com>
2024-10-13 12:23:04 +0400

    first commit

---
+++ Level 1/day 1/homework.py
@@ -0,0 +1,5 @@
+name = "dachi" 
+surname = "purtskhvanidze"
+age = 14
+height = 180.5
+print("My name is " + name + ". My surname is " + surname + ". I am " + str(14) + "years old, and my height is " + str(height))
---
+++ Level 1/day 1/lesson 1.py
@@ -0,0 +1,33 @@
+name = "dachi"
+ #name არის ცვლადი
+ #= არის მნიშვნელოვის მიმნიჭებელი სიმბოლო
+ #"dachi" არის ცვლადის მნიშვნელობა
+
+surname = "purtskhvanidze"
+
+# print(name)
+
+name = "dachi" #ეს არის str (stringi) ტიპის ცვლადი
+age = 22 #ეს არის int (integer) მთელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True
+is_ugly = False #snakecase(უბრალოდ წერის ხერხი სტანდარტული)
+
+isUgly = False #ჯავასკრიპტული camelcase
+
+
+
+# print(name + " " + surname )
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print(name + age)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
