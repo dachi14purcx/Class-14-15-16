04fdb3472540006317fa18152fce94d73792a62a
dachi14purcx <dachipurtskhvanidze2@gamil.com>
2024-12-17 11:29:11 +0400

    commit

--- level 19/home/homework.py
+++ level 19/home/homework.py
@@ -1,1 +1,15 @@
-#ჯერ არ არის ჩაგდებული
+nums =[]
+
+while len(nums) < 10:
+    num = int(input('num:'))
+    nums.append(num)
+
+print(max(nums))
+
+nums.remove(nums[2])
+
+nums.reverse()
+
+print(nums)
+
+print(nums.index(2445))
