c87bfc033b7f76716c5b9d245207078c5d78481a
dachi14purcx <dachipurtskhvanidze2@gamil.com>
2025-01-10 13:24:30 +0400

    commit

---
+++ level 70/class/classwork.js
@@ -0,0 +1,9 @@
+let name = 'დაჩი';
+let lastname = ' ფურცხვანიძე';
+let age = '14';
+let studyingPlace = 'GOA';
+
+console.log('მე მქვია ' + name + lastname + '. ვარ ' + age + ' წლის და ვსწავლობ ' + studyingPlace);
+
+alert('მე მქვია ' + name + lastname + '. ვარ ' + age + ' წლის და ვსწავლობ ' + studyingPlace);
+
---
+++ level 70/home/homework.js
@@ -0,0 +1,23 @@
+// 1
+let calculator = function(num1, num2){
+    let sum = num1 + num2;
+    let deduction = num1 -num2;
+    let devision = num1 / num2;
+    let multiply = num1 * num2;
+   return [sum,  deduction, devision, multiply]
+};
+console.log(calculator(1, 2));
+
+// 2
+let str = 'hello';
+let num = 1;
+let float = 2.45;
+
+// 3
+const string = 'hello';
+const number = 5;
+const flt = 1.3456;
+
+// let საშუალებით იქმნება ცვლადი რომელსაც შეუძლია შეიცვალოს მნიშვნელობა
+
+// const საშუალებით იქმნება ცვლადი რომელის მნიშვნელობის შეცვლაც შეიძლება
