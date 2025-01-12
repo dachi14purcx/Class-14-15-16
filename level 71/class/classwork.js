78e9b4d8484f2b575dac1f328f9dda8d61106e37
dachi14purcx <dachipurtskhvanidze2@gamil.com>
2025-01-12 20:18:17 +0400

    commit

---
+++ level 71/class/classwork.js
@@ -0,0 +1,18 @@
+// 1
+console.log(60 * 60 * 24 * 30)
+
+let seconds = function(){
+    let second = 60;
+    let minute = 60;
+    let hour = 24;
+    let day = 30;
+    return second * minute * hour * day
+}
+
+console.log(seconds());
+
+// 2
+let height =  14;
+let width = 18;
+
+console.log(height * width)
