8c1b0305cb214665623e03123339b3aaa2ef6af0
dachi14purcx <dachipurtskhvanidze2@gamil.com>
2025-02-12 19:57:39 +0400

    commmit

---
+++ level 090/class/classwork.js
@@ -0,0 +1,13 @@
+// 1
+let friends = new Map([
+    ["ნიკა", 25],
+    ["გიო", 30],
+    ["ლევანი", 28]
+]);
+
+console.log(friends);
+
+
+// 2
+let colors = new Set(["წითელი", "ლურჯი", "მწვანე", "ყვითელი", "პორფირის"]);
+console.log(colors.has("წითელი")); // true
